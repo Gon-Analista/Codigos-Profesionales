0b3c709009f9c7ae70008d8d58b6ea39d8445f34
nearQurita <go.ulloa@duocuc.cl>
2022-06-14 21:20:39 -0400

    opcion 2

--- funciones.py
+++ funciones.py
@@ -1,5 +1,5 @@
 valido = True
-nif_union = [] ; nif_not_union = []
+nif_union = [] ; nif_not_union = [] ; nombres  = [] ; edades = []
 def validar_menu():
     while valido:
         try:
@@ -11,19 +11,23 @@ def validar_menu():
         except ValueError:
             print("\nError: ingrese la opcion como un numero")
 
-def validar_opcion1():
+def validar_nif():
     while valido:
         nif = str(input("\nIngrese su NIF:\t"))
         if len(nif) == 12 and nif[0:8].isdigit() and nif[9:11].isalpha():
+            pertenece = "Usted pertenece a la union"
             nif_union.append(nif)
             break
         else:
-            print("\nError: debe ingresar un NIF valido, ejemplo: '99999999-RTX'")
+            pertenece = "Usted no pertenece a la union"
             nif_not_union.append(nif)
-
+            break
+    return nif_union,nif_not_union,pertenece
+def validar_opcion1():
     while valido:
         nombre = str(input("\nIngrese su nombre:\t"))
         if len(nombre) >= 8:
+            nombres.append(nombre)
             break
         else:
             print("\nError: Su nombre debe tener como minimo 8 caracteres")
@@ -31,11 +35,46 @@ def validar_opcion1():
         try:
             edad = int(input("\ningrese su edad:\t"))
             if edad > 0:
+                edades.append(edad)
                 break
             else:
                 print("\nError: Ingrese una edad mayor a 0")
         except ValueError:
             print("\nError: Ingrese su edad en numeros")
+    return nombres,edades,nombre
 
+def validar_opcion2(nombre):
+    if nombre != None:
+        buscar = str(input("\nIngrese el NIF de la persona que desea buscar:\t"))
+        while valido:
+            try:
+                if buscar in nif_not_union:
+                    index = nif_not_union.index(buscar)
+                    print("\nNIF:",nif_not_union[index])
+                    print("\nNombre:",nombres[index])
+                    print("\nEdad:",edades[index])
+                    print("\nEsta persona no pertenece a la union")
+                elif buscar in nif_union:
+                    print("\nNIF:",nif_not_union[index])
+                    print("\nNombre:",nombres[index])
+                    print("\nEdad:",edades[index])
+                    print("\nEsta persona no pertenece a la union")
+                else:
+                    print("\nError: no se encuentra en la lista")
+                break
+            except ValueError:
+                print("\nError: no se encuentra en la lista")
+                break
+    else:
+        print("\nError: No tenemos usuarios registrados, ingrese a la opcion 1")
 
-    return nif_union,nif_not_union,nombre,edad
+def validar_opcion3(nombre):
+    if nombre != None:
+        nacimiento = str(input("Ingrese su fecha de nacimiento: "))
+        estado = str(input("Ingrese su estado conyugal: "))
+        nif_union, nif_not_union, pertenece = validar_nif()
+        print(pertenece)
+     
+    
+    else:
+        print("\nError: No tenemos usuarios registrados, ingrese a la opcion 1")
--- prueba_3.py
+++ prueba_3.py
@@ -1,16 +1,18 @@
 from funciones import *
 menu = True
+nombre = None
+
 while menu:
     print("\n          Registro de ciudadados")
     print("\n1.Grabar\n2.Buscar\n3.Imprimir certificados\n4.Salir")
     opcion = validar_menu()
     if opcion == 1:
-        nif_union, nif_not_union, nombre, edad= validar_opcion1()
-        print(nif_union,nif_not_union,nombre,edad)
+        nif_union, nif_not_union, pertenece = validar_nif()
+        nombres, edades,nombre= validar_opcion1()
     elif opcion == 2:
-        print("op2")
+        validar_opcion2(nombre)
     elif opcion == 3:
-        print("op3")
+        validar_opcion3(nombre)
     elif opcion == 4:
         print("\nSaliendo del programa..\nAutor: Gonzalo Ulloa\nVersion: 1.0\n")
         break
